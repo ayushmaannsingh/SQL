b32a8f7c84732d97b3ba675748736f51f1a4de1a
ayushmaannsingh <sa776857@gmail.com>
2025-10-16 16:47:12 +0530

    Practice sql ques

---
+++ practice.sql
@@ -0,0 +1,33 @@
+CREATE DATABASE IF NOT EXISTS college;
+
+USE college;
+
+CREATE TABLE teacher (
+id INT PRIMARY KEY,
+name VARCHAR(50),
+subject VARCHAR(50),
+salary INT
+);
+
+INSERT INTO teacher 
+(id, name, subject, salary)
+VALUES
+(23, "ajay", "math", 50000),
+(47, "akash", "english", 60000),
+(18, "chetan", "chemistry", 45000),
+(9, "ayush", "physics", 75000);
+ 
+SELECT * FROM teacher;
+
+SELECT * FROM teacher 
+WHERE salary > 55000;
+
+ALTER TABLE teacher
+CHANGE COLUMN salary ctc INT;
+
+
+ALTER TABLE teacher 
+ADD COLUMN city VARCHAR(50) DEFAULT "Ranchi";
+
+ALTER TABLE teacher 
+DROP COLUMN ctc;
